3dd49aaf019da4d00fc8c4b82e341954a11ae3a3
Richardson Piper <817420@stma.k12.mn.us>
2022-04-07 12:26:35 -0500

    Access Granted and Denied

--- secret.js
+++ secret.js
@@ -1,3 +1,9 @@
- if document.getElementById("password").value == "the password") {
-	 innerHTML = "ACCESS GRANTED"
- }
+function password1() {
+	if(document.getElementById("password").value == "password") {
+	document.getElementById("check").innerHTML = "ACCESS GRANTED"
+}
+	else {
+		document.getElementById("check").innerHTML = "ACCESS DENIED"
+	}
+}
+document.getElementById("verify").addEventListener("click", password1)
